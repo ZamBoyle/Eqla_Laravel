2a396563d867faf44ed2d110501bb11a18d968b1
Johnny Piette <johnny.piette@gmail.com>
2023-06-27 14:25:15 +0200

    Updates

--- Installation/Composer-Setup.sh
+++ Installation/Composer-Setup.sh
@@ -9,15 +9,17 @@ register_shutdown_function(function() use (&\$ok) {
     exit(1);
   }
 });
+echo '- Téléchargement de l\'installeur'.PHP_EOL;
 copy('https://getcomposer.org/installer', 'composer-setup.php');
+echo '- Téléchargement de la signature pour le checksum'.PHP_EOL;
 copy('https://composer.github.io/installer.sig','composer-setup.php.sha');
 \$hash = trim(file_get_contents('composer-setup.php.sha'));
 if (hash_file('sha384', 'composer-setup.php') === \$hash) { 
     echo '- Checksum de l\'installeur est OK'.PHP_EOL;
-    echo '- Exécution de composer-setup.php'.PHP_EOL;
+    echo '- Exécution de l\'installeur composer-setup.php'.PHP_EOL;
     include 'composer-setup.php';
 } else { 
     echo '- L\'installeur est corrompu'.PHP_EOL;
     \$ok = false;
 }
-" && echo '- sudo mv composer.phar /usr/local/bin/composer' && sudo mv composer.phar /usr/local/bin/composer
+" && echo 'sudo mv composer.phar /usr/local/bin/composer' | tee /dev/tty | bash
